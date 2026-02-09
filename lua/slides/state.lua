0bc3db96ce301a4aa9ffef601277b8c2469824fb
Matt Riley <matt@mattriley.me>
2026-02-09 20:01:30 +0000

    Implement state module

--- lua/slides/state.lua
+++ lua/slides/state.lua
@@ -1,9 +1,14 @@
 -- lua/slides/state.lua
-local M = {}
+local M = { active=false, slides={}, current=1, buf=nil, win=nil, source_buf=nil }
 
 --- Reset state to initial values.
 function M.reset()
-  -- TODO: implement
+  M.active = false
+  M.slides = {}
+  M.current = 1
+  M.buf = nil
+  M.win = nil
+  M.source_buf = nil
 end
 
 return M
